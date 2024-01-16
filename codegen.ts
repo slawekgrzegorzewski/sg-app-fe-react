6edb5fe585f330691964e3472c05cddd526289f1
slawek <slawek.grz@gmail.com>
2024-01-16 21:45:01 +0100

    Change codegen.yml to codegen.ts

---
+++ codegen.ts
@@ -0,0 +1,23 @@
+import type {CodegenConfig} from '@graphql-codegen/cli'
+
+const config: CodegenConfig = {
+    schema: 'schema.graphql',
+    documents: "**/*.graphql",
+    generates: {
+        'src/types.ts': {
+            plugins: [
+                'typescript',
+                'typescript-resolvers',
+                'typescript-operations',
+                'typescript-document-nodes'
+            ],
+            config: {
+                scalars: {
+                    LocalDate: 'string'
+                }
+            }
+        }
+    }
+}
+
+export default config
