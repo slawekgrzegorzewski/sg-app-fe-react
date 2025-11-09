7ee502005f2c8548c4dc38e1590fa089bef4cf5e
slawek <slawek.grz@gmail.com>
2025-11-09 19:39:55 +0100

    bank-management

---
+++ src/accountant/settings/BanksManagement.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import {useQuery} from "@apollo/client";
+import {GetBankPermissions, GetBankPermissionsQuery} from "../../types";
+import {SimpleCrudList} from "../../application/components/SimpleCrudList";
+import {ComparatorBuilder} from "../../utils/comparator-builder";
+import {SxProps} from "@mui/system";
+import {Card, Theme} from "@mui/material";
+import {GQLBankPermission, mapBankPermission} from "../model/types";
+
+export function BanksManagement() {
+
+    const {
+        loading: bankAccountPermissionsLoading,
+        error: bankAccountPermissionsError,
+        data: bankAccountPermissionsData,
+        refetch: bankAccountPermissionsRefetch
+    } = useQuery<GetBankPermissionsQuery>(GetBankPermissions);
+    if (!bankAccountPermissionsData)
+        return <>
+        </>
+    else
+        return <SimpleCrudList
+            title={'Dostęp udzielony'}
+            list={[...bankAccountPermissionsData.bankPermissions.granted]
+                .map(mapBankPermission)
+                .sort(ComparatorBuilder.comparing<GQLBankPermission>(bankPermissions => bankPermissions.institutionId).build())}
+            idExtractor={bankPermission => bankPermission.publicId}
+            rowContainerProvider={(sx: SxProps<Theme>, additionalProperties: any) => {
+                return <Card sx={{marginBottom: '10px', ...sx}} {...additionalProperties}>
+                </Card>;
+            }}
+            entityDisplay={
+                bankPermission => {
+                    return <>{bankPermission.institutionId}</>
+                }
+            }
+            enableDndReorder={
+                false
+            }
+        />
+}
