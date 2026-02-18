fbebf0dbf919f6a8aa70de5ed35fcca70912035d
slawekgrzegorzewski <slawek.grzegorzewski@satago.com>
2026-02-18 10:31:39 +0100

    cosmetic changes

--- src/accountant/BankTransactionsImporter.tsx
+++ src/accountant/BankTransactionsImporter.tsx
@@ -2,31 +2,73 @@ import React, {useState} from "react";
 import {useQuery} from "@apollo/client/react";
 import {BankTransactionsToImport, BankTransactionsToImportQuery} from "../types";
 import Button from "@mui/material/Button";
-import PickDialog from "../utils/dialogs/PickDialog";
-import {GQLBankTransactionToImport, mapAccount, mapBankTransactionToImport} from "./model/types";
-import {SxProps} from "@mui/system";
-import {Stack, Theme} from "@mui/material";
+import {GQLAccount, GQLBankTransactionToImport, mapAccount, mapBankTransactionToImport} from "./model/types";
+import {Dialog, DialogContent, DialogTitle, Stack, useTheme} from "@mui/material";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import {formatCurrency} from "../utils/functions";
 import dayjs from "dayjs";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 
 export function BankTransactionsImporter() {
 
     const {loading, error, data, client} = useQuery<BankTransactionsToImportQuery>(BankTransactionsToImport);
     const [showDialog, setShowDialog] = useState(false);
-    const [targetBankAccountTransactionToImport, setTargetBankAccountTransactionToImport] = useState<GQLBankTransactionToImport | null>(null);
-
+    const [selectedBankAccountTransactionsToImport, setSelectedBankAccountTransactionsToImport] = useState<GQLBankTransactionToImport[]>([]);
+    const theme = useTheme();
     const reset = () => {
         client.clearStore();
         setShowDialog(false);
-        setTargetBankAccountTransactionToImport(null);
+        setSelectedBankAccountTransactionsToImport([]);
     }
 
     function transactionsToImportButtonText(transactionsCount: number) {
         return transactionsCount === 1
             ? "1 transakcja do zaimportowania"
             : transactionsCount + " transakcji do zaimportowania";
+    }
+
+    function onBankTransactionToImportClicked(bankTransactionToImport: GQLBankTransactionToImport) {
+        if (selectedBankAccountTransactionsToImport?.find(t => t.id === bankTransactionToImport.id)) {
+            setSelectedBankAccountTransactionsToImport([...selectedBankAccountTransactionsToImport?.filter(t => t.id !== bankTransactionToImport.id)]);
+        } else {
+            selectedBankAccountTransactionsToImport.push(bankTransactionToImport);
+            setSelectedBankAccountTransactionsToImport([...selectedBankAccountTransactionsToImport]);
+        }
+    }
+
+    function bankTransactionDisplay(accounts: GQLAccount[], bankTransactionToImport: GQLBankTransactionToImport) {
+        const sourceAccount = accounts.find(account => bankTransactionToImport.sourceAccountPublicId === account.publicId);
+        const destinationAccount = accounts.find(account => bankTransactionToImport.destinationAccountPublicId === account.publicId);
+        return (<Grid container
+                      key={bankTransactionToImport.id}
+                      sx={selectedBankAccountTransactionsToImport?.filter(t => t.id === bankTransactionToImport.id) ? {
+                          color: theme.palette.primary.contrastText,
+                          backgroundColor: theme.palette.primary.main,
+                      } : {}}
+                      onClick={() => {
+                          onBankTransactionToImportClicked(bankTransactionToImport);
+                      }}>
+            <Grid size={5}>
+                <Typography>Od: {sourceAccount?.name}</Typography>
+            </Grid>
+            <Grid
+                size={2}><Typography>{sourceAccount ? formatCurrency(sourceAccount.currentBalance.currency.code, bankTransactionToImport.debit) : ''}</Typography>
+            </Grid>
+            <Grid size={5}>
+                <Typography>Data:</Typography>
+            </Grid>
+            <Grid size={5}>
+                <Typography>Do: {destinationAccount?.name}</Typography>
+            </Grid>
+            <Grid size={2}>
+                <Typography>{destinationAccount ? formatCurrency(destinationAccount.currentBalance.currency.code, bankTransactionToImport.credit) : ''}</Typography>
+            </Grid>
+            <Grid size={5}>
+                <Typography>{dayjs(bankTransactionToImport.timeOfTransaction).locale('pl').format('DD MMMM')}</Typography>
+            </Grid>
+        </Grid>);
     }
 
     if (loading) {
@@ -39,43 +81,27 @@ export function BankTransactionsImporter() {
                 onClick={() => setShowDialog(true)}>{transactionsToImportButtonText(data.bankTransactionsToImport.length)}</Button>;
         } else {
             const accounts = data.financeManagement.accounts.map(mapAccount);
-            return <PickDialog
-                fullScreen={true}
-                title={'Wybierz transakcja do zaimportowania'}
-                options={data.bankTransactionsToImport.map(mapBankTransactionToImport)}
-                open={true}
-                onClose={() => reset()}
-                onPick={(value) => {
-                    setTargetBankAccountTransactionToImport(value);
-                    setShowDialog(false);
-                }}
-                idExtractor={function (bankTransactionToImport: GQLBankTransactionToImport): string {
-                    return bankTransactionToImport ? bankTransactionToImport.id + "" : "";
-                }}
-                descriptionExtractor={function (bankTransactionToImport: GQLBankTransactionToImport): string {
-                    return bankTransactionToImport ? (bankTransactionToImport.description) : "";
-                }}
-
-                containerProvider={(sx: SxProps<Theme>, additionalProperties: any) => {
-                    return <Stack directon={'column'} sx={{...sx}} {...additionalProperties}></Stack>;
-                }}
-
-                elementContainerProvider={(sx: SxProps<Theme>, additionalProperties: any, bankTransactionToImport: GQLBankTransactionToImport) => {
-                    const sourceAccount = accounts.find(account => bankTransactionToImport.sourceAccountPublicId === account.publicId)
-                    const destinationAccount = accounts.find(account => bankTransactionToImport.destinationAccountPublicId === account.publicId)
-                    return <Grid container sx={sx} {...additionalProperties} key={bankTransactionToImport.id}>
-                        <Grid size={5}><Typography>Od: {sourceAccount?.name}</Typography></Grid>
-                        <Grid
-                            size={2}><Typography>{sourceAccount ? formatCurrency(sourceAccount.currentBalance.currency.code, bankTransactionToImport.debit) : ''}</Typography></Grid>
-                        <Grid size={5}><Typography>Data:</Typography></Grid>
-                        <Grid size={5}><Typography>Do: {destinationAccount?.name}</Typography></Grid>
-                        <Grid
-                            size={2}><Typography>{destinationAccount ? formatCurrency(destinationAccount.currentBalance.currency.code, bankTransactionToImport.credit) : ''}</Typography></Grid>
-                        <Grid
-                            size={5}><Typography>{dayjs(bankTransactionToImport.timeOfTransaction).locale('pl').format('DD MMMM')}</Typography></Grid>
-                    </Grid>
-                }}
-            />;
+            return <Dialog onClose={() => reset()}
+                           open={true}
+                           fullScreen={true}>
+                <DialogTitle onClick={e => e.stopPropagation()}>
+                    <Stack direction={'row'} justifyContent={'space-between'}>
+                        <Typography variant={"h4"}>Wybierz transakcja do zaimportowania</Typography>
+                        <IconButton onClick={() => reset()}>
+                            <CloseIcon/>
+                        </IconButton>
+                    </Stack>
+                </DialogTitle>
+                <DialogContent onClick={e => e.stopPropagation()}>
+                    <Stack>
+                        {
+                            (data.bankTransactionsToImport
+                                .map(mapBankTransactionToImport)
+                                .map(bankTransactionToImport => bankTransactionDisplay(accounts, bankTransactionToImport)))
+                        }
+                    </Stack>
+                </DialogContent>
+            </Dialog>
         }
     } else {
         return <></>;
