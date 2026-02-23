86efd4efa5f4eff2f2933d73100a59b29e2ecf20
slawek <slawek.grz@gmail.com>
2026-02-23 22:18:37 +0100

    start of rubiks cube

--- src/cubes/CubesMainPage.tsx
+++ src/cubes/CubesMainPage.tsx
@@ -2,30 +2,26 @@ import * as React from "react";
 import {useEffect, useRef, useState} from "react";
 import {Box, Stack} from "@mui/material";
 import {newCube} from "./visualizer";
-import scramble from "./cube-scrambler";
+import {scramble as generateScramble} from "./cube-scrambler";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 
 export function CubesMainPage() {
-    const [scr, setScr] = useState("");
-    const ref = useRef<HTMLElement | null>(null);
+    const [scramble, setScramble] = useState("");
+    const cubeVisualizationContainerRef = useRef<HTMLElement | null>(null);
 
     useEffect(() => {
-        const s = newCube(ref.current!, 3);
+        const s = newCube(cubeVisualizationContainerRef.current!, 3);
         s.enableKey = (_) => true;
-        s.puzzle.performAlg(scr);
-    }, [scr])
+        s.puzzle.performAlg(scramble);
+    }, [scramble]);
 
-    const makeScramble = () => {
-        setScr(scramble({turns: 30}).join(" "));
-    }
-
-    return <Stack dir={'column'}>
-        <Box component="div" ref={ref}
+    return <Stack dir={'column'} alignItems={'center'}>
+        <Button onClick={() => setScramble(generateScramble({turns: 30}).join(" "))}>Scramble</Button>
+        <Typography variant={'h5'}>{scramble}</Typography>
+        <Box component="div" ref={cubeVisualizationContainerRef}
              id="scenesContainer"
              sx={{display: 'flex', flexWrap: 'wrap', gap: '16px', width: '300px', height: '300px'}}>
         </Box>
-        <Typography>{scr}</Typography>
-        <Button onClick={makeScramble}>Scramble</Button>
     </Stack>;
 }
